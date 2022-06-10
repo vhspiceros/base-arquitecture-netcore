103ee6120a654bed00d8c26b9c7c76bfce46cd62
Victor Hugo Saavedra <vhspiceros@gmail.com>
2022-06-10 15:38:54 -0400

    Initial commit

---
+++ src/AspnetRun.Application/Mapper/ObjectMapper.cs
@@ -0,0 +1,35 @@
+﻿using AspnetRun.Application.Models;
+using AspnetRun.Core.Entities;
+using AutoMapper;
+using System;
+
+namespace AspnetRun.Application.Mapper
+{
+    // The best implementation of AutoMapper for class libraries -> https://www.abhith.net/blog/using-automapper-in-a-net-core-class-library/
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                // This line ensures that internal properties are also mapped over.
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<AspnetRunDtoMapper>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+        public static IMapper Mapper => Lazy.Value;
+    }
+
+    public class AspnetRunDtoMapper : Profile
+    {
+        public AspnetRunDtoMapper()
+        {
+            CreateMap<Product, ProductModel>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName)).ReverseMap();
+
+            CreateMap<Category, CategoryModel>().ReverseMap();
+        }
+    }
+}
